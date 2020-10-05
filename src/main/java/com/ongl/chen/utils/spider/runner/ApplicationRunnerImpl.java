a2f93ee1429dd1432d9c12609c54fa8ad4a6ec44
龙茂靖 <maojinglong@caih.com>
2020-10-05 13:25:02 +0800

    把京东商品跑通，还能用

---
+++ src/main/java/com/ongl/chen/utils/spider/runner/ApplicationRunnerImpl.java
@@ -0,0 +1,34 @@
+package com.ongl.chen.utils.spider.runner;
+
+import com.ongl.chen.utils.spider.processor.JDProductProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lnj
+ * createTime 2018-11-07 22:37
+ **/
+@Component
+public class ApplicationRunnerImpl implements ApplicationRunner {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private JDProductProcessor jdProductProcessor;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("通过实现ApplicationRunner接口，在spring boot项目启动后打印参数");
+        String[] sourceArgs = args.getSourceArgs();
+        for (String arg : sourceArgs) {
+            System.out.print(arg + " ");
+        }
+        jdProductProcessor.start();
+        System.out.println();
+    }
+}
